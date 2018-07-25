bda07c5fcca5c246c1607bf99510bb54d5ceab54
张洁 <zhangjie@zcckj.com>
2018-07-25 10:26:00 +0800

    数据库脚本修改

--- sql-scripts/data.sql
+++ sql-scripts/data.sql
@@ -2,8 +2,8 @@ INSERT INTO `test`.`t_sys_user` (`id`, `username`, `password`) VALUES ('1', 'jac
 INSERT INTO `test`.`t_sys_user` (`id`, `username`, `password`) VALUES ('2', 'lily', '123455');
 INSERT INTO `test`.`t_sys_user` (`id`, `username`, `password`) VALUES ('3', 'admin', '123456');
 
-INSERT INTO `test`.`t_sys_role` (`id`, `name`) VALUES ('1', 'admin');
-INSERT INTO `test`.`t_sys_role` (`id`, `name`) VALUES ('2', 'operater');
+INSERT INTO `test`.`t_sys_role` (`id`, `name`) VALUES ('1', 'ROLE_admin');
+INSERT INTO `test`.`t_sys_role` (`id`, `name`) VALUES ('2', 'ROLE_operater');
 
 
 INSERT INTO `test`.`t_sys_user_role` (`id`, `sys_user_id`, `sys_role_id`) VALUES ('1', '1', '1');
